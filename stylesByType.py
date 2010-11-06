0c0ad073b6ef1cfea66cd2e109988e16ed8f0bce
Christopher Scott Hernandez <c.scott.hernandez@gmail.com>
2010-11-06 09:46:51 -0400

    reset rulegroup to empty at each iteration

--- stylesByType.py
+++ stylesByType.py
@@ -19,26 +19,38 @@ def main(argv):
     
     # wondering if there should be a check here
     # for a valid regex... or otherwise validate this argument
-    pattern = re.compile( r"(" + argv[1] + r".*?;)")
+    pattern = re.compile( r"(" + argv[1] + r".*?)(?:;|\})")
     
     # start the process
     # ideally after input has been validated/checked
     buildDict(source, pattern)
 
 def buildDict(source, pattern):
-  """ Build a dictionary of selector => rule string pairs
+  """ Build a dictionary of selector => rule string pairs.
   
-  Should, at a minimum return a status to the environment from which it was called"""
+  Should, at a minimum return a status to the environment from which it was called. """
+  
+  rulegroup = ""
+  selectorEnd = 0
+  
   for line in source.readlines():
+    
     if reSelector.match(line):
       selector = reSelector.match(line)
-    if pattern.search(line):
-      match = pattern.search(line)
-      if selector:
-        mydict[selector.group(1)] = match.group(1)
+      selectorEnd = selector.end(1)
+      
+    if pattern.search(line, selectorEnd):
+      for match in pattern.finditer(line):        
+        if match.group(1) == selector.group(1): continue
+        rulegroup += "  " + str(match.group(1)).rstrip() + ";\n"
+      mydict[selector.group(1)] = rulegroup
 
+    rulegroup = ""
+  
+  # Output; needs to be refactored to give the option 
+  # of writing to file, otherwise: std out.     
   for selector, rule in mydict.items():
-    print("%s { \n %s \n } \n" % (selector, rule))
+    print("%s { \n%s}\n" % (selector, rule))
     
 if __name__ == "__main__":
   main(sys.argv[1:])
