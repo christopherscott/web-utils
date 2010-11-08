6a7c2ca2eb9d09c94ee0b305738352361c7cd864
Christopher Scott Hernandez <c.scott.hernandez@gmail.com>
2010-11-07 21:11:46 -0500

    added support for command line args in kill empties

--- killEmptyStyles.py
+++ killEmptyStyles.py
@@ -1,15 +1,33 @@
 #!/usr/bin/env python
 import re
 import sys
+import getopt
 
 def main(argv):
   try:
-    source = open(argv[0]).read()
-  except:
-    print "%s could not be opened" % argv
-  killEmpties(source)
+    opts, args = getopt.getopt(argv, "hdo:", ["help", "debug"])
+  except getopt.GetoptError:
+    usage()
+    sys.exit(2)
   
-
+  for i in args:
+    print i
+  
+  print "====="
+  
+  for opt, arg in opts:
+    if opt in ("-h", "--help"):
+      usage()
+      sys.exit()
+    elif opt in ("-d", "--debug"):
+      global _debug
+      _debug = 1
+    elif opt in ("-o", "--output"):
+      print "output"
+    
+  
+def usage():
+  print "Usage: killEmptyStyles.py css-file [-o output-file]"
 
 def killEmpties(source):
   emptyRule = re.compile(r".*\{(\s)*\}")
